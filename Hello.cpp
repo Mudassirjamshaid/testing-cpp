9c938cd2248f44917af09f166b979f476ee16d08
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-21 07:38:54 +0500

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -3,11 +3,27 @@
 #include <ctime>
 #include <cmath>
 #include <string>
-
 using namespace std;
+
+class Hello
+{
+private:
+    /* data */
+public:
+
+    void getInfo() {
+        static int x = 0;
+        cout << "X  : " << x << endl;
+        x++;
+
+    }
+};
 
 int main()
 {
-    cout << "Hello World";
+    Hello s;
+    s.getInfo();
+    s.getInfo();
+    s.getInfo();    
     return 0;
 }
