3a4252210d86eb1eaf09db9845175eee9da6242e
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-21 08:17:18 +0500

    Virtual Function

--- Hello.cpp
+++ Hello.cpp
@@ -1,29 +1,47 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
-#include <string>
 using namespace std;
 
-class Hello
-{
-private:
-    /* data */
-public:
 
-    void getInfo() {
-        static int x = 0;
-        cout << "X  : " << x << endl;
-        x++;
+class Shape {
+   protected:
+    float dimension;
 
+   public:
+    void getDimension() {
+        cin >> dimension;
+    }
+
+    // pure virtual Function
+    virtual float calculateArea() = 0;
+};
+
+// Derived class
+class Square : public Shape {
+   public:
+    float calculateArea() {
+        return dimension * dimension;
     }
 };
 
-int main()
-{
-    Hello s;
-    s.getInfo();
-    s.getInfo();
-    s.getInfo();    
+// Derived class
+class Circle : public Shape {
+   public:
+    float calculateArea() {
+        return 3.14 * dimension * dimension;
+    }
+};
+
+int main() {
+    Square square;
+    Circle circle;
+
+    cout << "Enter the length of the square: ";
+    square.getDimension();
+    cout << "Area of square: " << square.calculateArea() << endl;
+
+    cout << "\nEnter radius of the circle: ";
+    circle.getDimension();
+    cout << "Area of circle: " << circle.calculateArea() << endl;
+
     return 0;
 }
