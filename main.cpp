e7709824ff7399b4608e3a203e9bcf0c6450c880
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-23 20:42:37 +0500

    template

--- main.cpp
+++ main.cpp
@@ -2,21 +2,45 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
-#include <list>
+#include <string>
 
 using namespace std;
 
+template <typename T>
+class CalculateValue {
+  public:
+
+  T add(T a, T b) {
+    return a + b;
+  }
+  
+  T substract(T a, T b) {
+    return a - b;
+  }
+  
+  T multiply(T a, T b) {
+    return a * b;
+  }
+
+  
+  T divides(T a, T b) {
+    if (b == 0)
+    {
+      cout << "Zero Cannot be Allowed To Divide Valve ";
+    }
+    
+    return a / b;
+  }
+};
+
 int main()
 {
-  list<int> mylist;
-  mylist.push_back(12);
-  mylist.push_back(22);
-  mylist.push_front(9);
-  mylist.erase( mylist.begin());
+  CalculateValue<int> intvalue;
+  intvalue.add(5, 6);
+  intvalue.substract(5, 9);
 
-for(list<int>::iterator it = mylist.begin(); it != mylist.end(); it++) {
-  cout << *it << endl;
-}
-  
+  CalculateValue<float> floatvalue;
+  floatvalue.multiply(7, 8);
+  floatvalue.divides(6, 9);
   return 0;
 }
