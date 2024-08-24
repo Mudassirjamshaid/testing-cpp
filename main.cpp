d4c6085c27ef93c57b197817863d0917877f258d
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-24 11:32:20 +0500

    list

--- main.cpp
+++ main.cpp
@@ -4,19 +4,16 @@
 int main() {
     std::list<std::string> myList = {"apple", "banana", "cherry"};
 
-    // Displaying list using iterator
     std::cout << "Fruits: ";
     for (std::list<std::string>::iterator it = myList.begin(); it != myList.end(); ++it) {
         std::cout << *it << " ";
     }
     std::cout << std::endl;
 
-    // Inserting an element using an iterator
     std::list<std::string>::iterator it = myList.begin();
-    ++it;  // Move the iterator to the second position
+    ++it; 
     myList.insert(it, "orange");
 
-    // Displaying the updated list
     std::cout << "Updated Fruits: ";
     for (const auto& fruit : myList) {
         std::cout << fruit << " ";
