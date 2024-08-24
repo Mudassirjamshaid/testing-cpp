c342b126b74de89ee1d72d274ccbe4077e070921
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-24 09:23:13 +0500

    list sorting

--- Hello.cpp
+++ Hello.cpp
@@ -2,28 +2,13 @@
 #include <list>
 
 int main() {
-    std::list<int> myList;  // Create an empty list of integers
+    std::list<int> myList = {30, 10, 20, 50, 40};
 
-    // Adding elements to the list
-    myList.push_back(10);
-    myList.push_back(20);
-    myList.push_back(30);
+    // Sorting the list
+    myList.sort();
 
-    // Displaying the list elements
-    std::cout << "List elements: ";
-    for (int value : myList) {
-        std::cout << value << " ";
-    }
-    std::cout << std::endl;
-
-    // Inserting an element at the beginning
-    myList.push_front(5);
-
-    // Removing the last element
-    myList.pop_back();
-
-    // Displaying the updated list elements
-    std::cout << "Updated List elements: ";
+    // Displaying the sorted list
+    std::cout << "Sorted List: ";
     for (int value : myList) {
         std::cout << value << " ";
     }
