90b9b61b93d0a155f3c02944d756cfb834d86fac
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-24 08:42:58 +0500

    list Example

--- Hello.cpp
+++ Hello.cpp
@@ -1,91 +1,28 @@
 #include <iostream>
+#include <list>
+
 using namespace std;
 
-class Person {
-public:
-    string name;
-    int age;
-
-    virtual void getdata() = 0;
-    virtual void putdata() = 0;
-};
-
-class Professor : public Person {
-public:
-    int publications;
-    static int idCounter;
-    int cur_id;
-
-    Professor() {
-        cur_id = ++idCounter;
-    }
-
-    void getdata() override {
-        cin >> name >> age >> publications;
-    }
-
-    void putdata() override {
-        cout << name << " " << age << " " << publications << " " << cur_id << endl;
-    }
-};
-
-// Initialize static member
-int Professor::idCounter = 0;
-
-class Student : public Person {
-public:
-    int marks[6];
-    static int idCounter;
-    int cur_id;
-
-    Student() {
-        cur_id = ++idCounter;
-    }
-
-    void getdata() override {
-        cin >> name >> age;
-        for (int i = 0; i < 6; i++) {
-            cin >> marks[i];
-        }
-    }
-
-    void putdata() override {
-        int sum = 0;
-        for (int i = 0; i < 6; i++) {
-            sum += marks[i];
-        }
-        cout << name << " " << age << " " << sum << " " << cur_id << endl;
-    }
-};
-
-// Initialize static member
-int Student::idCounter = 0;
-
 int main() {
-    int n, val;
-    cin >> n; // The number of objects that are going to be created.
-    Person *per[n];
-
-    for (int i = 0; i < n; i++) {
-        cin >> val;
-        if (val == 1) {
-            // If val is 1, the current object is of type Professor
-            per[i] = new Professor;
-        } else {
-            // Else, the current object is of type Student
-            per[i] = new Student;
-        }
-
-        per[i]->getdata(); // Get the data from the user.
+    // create a list
+    list<int> numbers = {1, 2, 3, 4, 5};
+ 
+    // display the original list 
+    cout << "Inital List: ";
+    for(int number : numbers) {
+        cout << number << ",  ";
     }
 
-    for (int i = 0; i < n; i++) {
-        per[i]->putdata(); // Print the required output for each object.
-    }
+    // remove the first element of the list
+    numbers.pop_front();
 
-    // Clean up dynamic memory
-    for (int i = 0; i < n; i++) {
-        delete per[i];
+    // remove the last element of the list
+    numbers.pop_back();
+
+    // display the modified list 
+    cout << endl << "Final List: ";
+    for(int number : numbers) {
+        cout << number << ", ";
     }
 
     return 0;
