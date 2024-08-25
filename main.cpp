94caeb1edae8b2d054ef97d284fbe5a5bb5e19fd
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-25 10:37:57 +0500

    myclass

--- main.cpp
+++ main.cpp
@@ -1,16 +1,88 @@
 #include <iostream>
-#include <array>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
-int main(){
-    array<int, 5> a;
+class PersonInfo
+{
+private:
+    float salery;
+    string password;
+    float price;
+public:
+    string name;
+    int age;
+    string address;
+    string state;
+    int stander;
+    float cgpa;
+    int id;
+    string city;
+    string fatherName;
+    int rollNo;
+    string subject;
+    string department;
 
-    // fill the array with zeros
-    a.fill(0);
-  
-    cout << "The elements are: ";
-    for(const int& elt: a){
-        cout << elt << " ";
+    PersonInfo();
+    ~PersonInfo();
+
+    void input() {
+        cout << "Enter Your Name: ";
+        cin >> name;
+        cout << "Enter Your Age: ";
+        cin >> age;
+        cout << "Enter Your Subject: ";
+        cin >> subject;
+        cout << "Enter Your Salary: ";
+        cin >> salery;
+        cout << "Enter Your Password: ";
+        cin >> password;
     }
+
+    friend class Hello;
+};
+
+PersonInfo::PersonInfo() {
+    // Constructor implementation
+}
+
+PersonInfo::~PersonInfo() {
+    // Destructor implementation
+}
+
+class Hello
+{
+public:
+    Hello();
+    ~Hello();
+
+    void print(PersonInfo &obj) {
+        cout << "Name: " << obj.name << endl;
+        cout << "Age: " << obj.age << endl;
+        cout << "Subject: " << obj.subject << endl;
+        cout << "Salary: " << obj.salery << endl;
+        cout << "Password: " << obj.password << endl;
+    }
+};
+
+Hello::Hello() {
+    // Constructor implementation
+}
+
+Hello::~Hello() {
+    // Destructor implementation
+}
+
+int main()
+{
+    PersonInfo person;
+    person.input();
+
+    Hello hello;
+    hello.print(person);
+
+    return 0;
 }
