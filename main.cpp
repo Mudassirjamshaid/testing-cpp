2c8f2bf8d7baf4903b728e9223bc63348d2798ff
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-25 08:36:44 +0500

    testing-array-featurs

--- main.cpp
+++ main.cpp
@@ -6,11 +6,23 @@ using namespace std;
 int main() {
     array<string, 5> numbers {"apple", "Mango", "banana", "cherry", "flower"};
     
-    cout << "String array : ";
+       cout << "String array : ";
     for (string number : numbers) 
     {
-        cout << number << " " ;
+        cout << number << " " << endl;
     }
     
+
+    
+        if (numbers.empty())
+        {
+            cout << "Your Array is empty You Nead add some value in your array cantainer Thank You ";
+        }
+        else {
+             cout << "Your Array Cantainer is not empty It Cantain Some Value Check it out " << endl;
+
+        }
+    
+   
     return 0;
 }
