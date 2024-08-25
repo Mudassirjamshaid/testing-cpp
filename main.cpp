269fc9dbdcf04e7b2327052b7b2cda95b12a3d43
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-25 08:45:20 +0500

    array

--- main.cpp
+++ main.cpp
@@ -3,26 +3,14 @@
 
 using namespace std;
 
-int main() {
-    array<string, 5> numbers {"apple", "Mango", "banana", "cherry", "flower"};
-    
-       cout << "String array : ";
-    for (string number : numbers) 
-    {
-        cout << number << " " << endl;
+int main(){
+    array<int, 5> a;
+
+    // fill the array with zeros
+    a.fill(0);
+  
+    cout << "The elements are: ";
+    for(const int& elt: a){
+        cout << elt << " ";
     }
-    
-
-    
-        if (numbers.empty())
-        {
-            cout << "Your Array is empty You Nead add some value in your array cantainer Thank You ";
-        }
-        else {
-             cout << "Your Array Cantainer is not empty It Cantain Some Value Check it out " << endl;
-
-        }
-    
-   
-    return 0;
 }
