7bf5a7efcf63460ca00d65ba77647ee6c64ee47b
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-25 08:12:33 +0500

    testing an aray

--- main.cpp
+++ main.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
-#include <algorithm>
-#include <numeric>
 #include <array>
 
 using namespace std;
 
-int main(){
-    array<int, 5> age = {45, 23, 66, 87, 21};
-
-    // sorting
-    sort(age.begin(), age.end());
-
-    // print the sorted array
-    cout << "The sorted array is: ";
-    for(const int elt: age){
-        cout << elt << " ";
+int main() {
+    array<int, 5> numbers {1, 2, 3, 4, 5};
+    
+    cout << "Testing an array ";
+    for (int number : numbers) 
+    {
+        cout << number << " " ;
     }
-    cout << endl;
-
-    // searching
-   // checking whether the number 5  exists or not
-    auto found = find(age.begin(), age.end(), 5);
-
-    if (found != age.end()) cout << "5 was Found" << endl;
-    else cout << "5 was Not Found" << endl;
-
-    // summing
-    int sum = accumulate(age.begin(), age.end(), 0);
-
-    cout << "The sum of the element of array is : " << sum;
+    
+    return 0;
 }
