2fe85add90c9c295ed94dae5149b3bb560c37580
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-27 15:42:45 +0500

    Getting Start with stack

--- Hello.cpp
+++ Hello.cpp
@@ -1,40 +1,18 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <vector>
+#include <stack>
 #include <string>
 
 using namespace std;
 
-
-
 int main()
 {
-    vector<int> numbers;
-    
-    try{
-        if (numbers.empty())
-        {
-            cout << "Your vector is empty you need to enter some data in your vector : " << endl;
-        }
-        else {
-            throw(numbers);
-        }
-    }
-    catch(int number) {
-        cout << "Your Vector is not Empty it cantain some value : " << endl;
-    }
+    stack<string> stack;
+    stack.push("Cpp");
+    stack.push("Python");
+    stack.push("Java");
 
-    numbers.push_back(13);
-    numbers.push_back(12);
-
-    for (int number : numbers )
-    {
-        cout << number << " " << endl;
-    }
-
-    cout << "capisty : " << numbers.capacity() << endl;
-    cout << numbers.size() << " bytes" << endl;
-    
+    cout << stack.top() << endl;
     return 0;
 }
