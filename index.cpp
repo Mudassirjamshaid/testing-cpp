65b197a2742e0e23785f30eb01f72a560c974184
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-28 07:16:17 +0500

    empty-queue

--- index.cpp
+++ index.cpp
@@ -6,15 +6,32 @@ int main() {
 
   // create a queue of string
   queue<string> languages;
+  
+  cout << "Is the queue empty? ";
+
+  // check if the queue is empty  
+  if (languages.empty()) {
+    cout << "Yes" << endl;
+  }
+  else {
+    cout << "No" << endl;
+  }
+
+  cout << "Pushing elements..." << endl;
 
   // push element into the queue
   languages.push("Python");
   languages.push("C++");
-  languages.push("Java");
-  
-  // get the size of the queue
-  int size = languages.size();
-  cout << "Size of the queue: " << size;
+ 
+  cout << "Is the queue empty? ";
+
+  // check if the queue is empty  
+  if (languages.empty()) {
+    cout << "Yes";
+  }
+  else {
+    cout << "No";
+  }
 
   return 0;
 }
