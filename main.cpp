3443ef02646a546785361ab20bf71ef27d0ec167
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-28 17:52:54 +0500

    Updation

--- main.cpp
+++ main.cpp
@@ -1,25 +1,99 @@
 #include <iostream>
-#include <list>
-#include <map>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+template <typename T>
+class Person {
+private:
+  T salary;
+  T password;
+
+public:
+  T name;
+  T age;
+  T gender;
+  T address;
+  T phone;
+  T email;
+  T birthday;
+  T occupation;
+  T education;
+  T skills;
+  T languages;
+  T hobbies;
+  T interests;
+  T goals;
+  T ambitions;
+  T dreams;
+  T subject;
+  T marks;
+  T grades;
+
+  // Declare the friend function to access private members
+  template <typename U>
+  friend void printPrivateInfo(const Person<U>& person);
+
+  // Pure virtual functions
+  virtual void input() = 0;
+  virtual void display() = 0;
+};
+
+template <typename T>
+void printPrivateInfo(const Person<T>& person) {
+  cout << "Salary: " << person.salary << endl;
+  cout << "Password: " << person.password << endl;
+}
+
+template <typename T>
+class Employes : public Person<string> {
+public:
+  void input() override {
+    cout << "Enter name: ";
+    getline(cin, name);
+    cout << "Enter age: ";
+    getline(cin, age);
+    cout << "Enter gender: ";
+    getline(cin, gender);
+    cout << "Enter address: ";
+    getline(cin, address);
+    cout << "Enter education: ";
+    getline(cin, education);
+    cout << "Enter skills: ";
+    getline(cin, skills);
+  }
+
+  void display() override {
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Gender: " << gender << endl;
+    cout << "Address: " << address << endl;
+    cout << "Education: " << education << endl;
+    cout << "Skills: " << skills << endl;
+  }
+};
+
 int main() {
-    map<string, list<string>> mystring;
-    list<string> cats {"oggy", "Jack", "olivia", "tom", "jerry"};
-    list<string> dogs {"BOB", "dog2", "dog3", "dog4" };
-    list<string> cockroach {"Dee Dee", "Marky", "Joey"};
+  Employes<string> stringValues;
+  stringValues.input();
 
-    mystring.insert(pair<string, list<string>>("cats", cats));
-    mystring.insert(pair<string, list<string>>("dogs", dogs));
-    mystring.insert(pair<string, list<string>>("cockroach", cockroach));
+  ofstream file("Text.txt");
+  if (file.is_open()) {
+    file << "Information about Employes" << endl;
+    file << "Name: " << stringValues.name << endl;
+    file << "Age: " << stringValues.age << endl;
+    file << "Gender: " << stringValues.gender << endl;
+    file << "Address: " << stringValues.address << endl;
+    file << "Education: " << stringValues.education << endl;
+    file << "Skills: " << stringValues.skills << endl;
+    file.close();
+  } else {
+    cout << "Unable to open file" << endl;
+  }
 
-    for(auto pair : mystring) {
-        cout << pair.first << ": ";
-        for(auto animal : pair.second) {
-            cout << animal << " ";
-        }
-        cout << endl;
-    }
-    return 0;
+  // Print private information using the friend function
+  printPrivateInfo(stringValues);
+
+  return 0;
 }
