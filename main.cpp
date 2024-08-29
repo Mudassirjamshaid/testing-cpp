676e47ae2e2af94ecc8a1eec2d818b2640811a9e
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-29 06:15:38 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,154 +1,26 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-
+#include <map>
 using namespace std;
 
-template <typename T>
-class Person {
-private:
-  T salary;
-  T password;
-
-public:
-  T name;
-  T age;
-  T gender;
-  T address;
-  T phone;
-  T email;
-  T birthday;
-  T occupation;
-  T education;
-  T skills;
-  T languages;
-  T hobbies;
-  T interests;
-  T goals;
-  T ambitions;
-  T dreams;
-  T subject;
-  T marks;
-  T grades;
-
-  // Declare the friend function to access private members
-  template <typename U>
-  friend void printPrivateInfo(const Person<U>& person);
-
-  // Pure virtual functions
-  virtual void input() = 0;
-  virtual void display() = 0;
-};
-
-template <typename T>
-void printPrivateInfo(const Person<T>& person) {
-  cout << "Salary: " << person.salary << endl;
-  cout << "Password: " << person.password << endl;
-}
-
-template <typename T>
-class Employes : public Person<string> {
-public:
-  void input() override {
-    cout << "Enter name: ";
-    getline(cin, name);
-    cout << "Enter age: ";
-    getline(cin, age);
-    cout << "Enter gender: ";
-    getline(cin, gender);
-    cout << "Enter address: ";
-    getline(cin, address);
-    cout << "Enter education: ";
-    getline(cin, education);
-    cout << "Enter skills: ";
-    getline(cin, skills);
-  }
-
-  void display() override {
-    cout << "Name: " << name << endl;
-    cout << "Age: " << age << endl;
-    cout << "Gender: " << gender << endl;
-    cout << "Address: " << address << endl;
-    cout << "Education: " << education << endl;
-    cout << "Skills: " << skills << endl;
-  }
-};
-
-class Student : public Person<string> {
-public:
-  void input() override {
-    cout << "Enter name: ";
-    getline(cin, name);
-    cout << "Enter age: ";
-    getline(cin, age);
-    cout << "Enter gender: ";
-    getline(cin, gender);
-    cout << "Enter address: ";
-    getline(cin, address);
-    cout << "Enter subject: ";
-    getline(cin, subject);
-    cout << "Enter marks: ";
-    getline(cin, marks);
-    cout << "Enter grades: ";
-    getline(cin, grades);
-    cout << "Enter your interests: ";
-    getline(cin, interests);
-    cout << "What is your goal: ";
-    getline(cin, goals);
-  }
-
-  void display() override {
-    cout << "Name: " << name << endl;
-    cout << "Age: " << age << endl;
-    cout << "Gender: " << gender << endl;
-    cout << "Address: " << address << endl;
-    cout << "Subject: " << subject << endl;
-    cout << "Marks: " << marks << endl;
-    cout << "Grades: " << grades << endl;
-    cout << "Interests: " << interests << endl;
-    cout << "Goals: " << goals << endl;
-  }
-};
-
 int main() {
-  // Employee information
-  Employes<string> employee;
-  employee.input();
+    
+    map<int, string> student;
+  
+    // use [] operator to add elements
+    student[1] = "Jacqueline";
+    student[2] = "Blake";
 
-  ofstream file("Text.txt");
-  if (file.is_open()) {
-    file << "Information about Employees" << endl;
-    file << "Name: " << employee.name << endl;
-    file << "Age: " << employee.age << endl;
-    file << "Gender: " << employee.gender << endl;
-    file << "Address: " << employee.address << endl;
-    file << "Education: " << employee.education << endl;
-    file << "Skills: " << employee.skills << endl;
-    file.close();
-  } else {
-    cout << "Unable to open file" << endl;
-  }
+    // use insert() method to add elements
+    student.insert(make_pair(3, "Denise"));
+    student.insert(make_pair(4, "Blake"));
 
-  // Student information
-  Student<string> student;
-  student.input();
+    // add elements with duplicate keys
+    student[5] = "Timothy";
+    student[5] = "Aaron";
 
-  file.open("Text.txt", ios::app);  // Open file in append mode
-  if (file.is_open()) {
-    file << "\nInformation about Students" << endl;
-    file << "Name: " << student.name << endl;
-    file << "Age: " << student.age << endl;
-    file << "Gender: " << student.gender << endl;
-    file << "Address: " << student.address << endl;
-    file << "Subject: " << student.subject << endl;
-    file << "Marks: " << student.marks << endl;
-    file << "Grades: " << student.grades << endl;
-    file << "Interests: " << student.interests << endl;
-    file << "Goals: " << student.goals << endl;
-    file.close();
-  } else {
-    cout << "Unable to open file" << endl;
-  }
+    for (int i = 1; i <= student.size(); ++i) {
+        cout << "Student[" << i << "]: " << student[i] << endl;
+    }
 
-  return 0;
+    return 0;
 }
