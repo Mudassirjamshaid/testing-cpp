c6eb976d6b44bf3bc21001e55d6ff71d3f87c25d
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-29 12:42:19 +0500

    canstractor overriding

--- Hello.cpp
+++ Hello.cpp
@@ -2,33 +2,60 @@
 #include <stack>
 using namespace std;
 
-class Person
-{
+class Person {
 private:
-  /* data */
+  // Private data members can be added here if needed
+
 public:
-   string name;
-   int age;
+  string name;
+  int age;
+
+  // Constructor
   Person(string name, int age);
+
+  // Destructor
   ~Person();
 };
 
-Person::Person(string name, int age)
-{
+Person::Person(string name, int age) {
   this->name = name;
-  this->age = age; 
+  this->age = age;
 }
 
-Person::~Person()
-{
-  cout << "destractor is playing...\n";
+Person::~Person() {
+  cout << "Destructor is called for Person: " << name << endl;
 }
 
-int main()
-{
-  Person p("mudasir", 17);
+class Student : public Person {
+public:
+  int rollno;
 
-  cout << "name: " << p.name << endl;
+  // Constructor for Student, taking name, age, and rollno
+  Student(string name, int age, int rollno);
+
+  // Destructor
+  ~Student();
+};
+
+Student::Student(string name, int age, int rollno) : Person(name, age) {
+  this->rollno = rollno;
+}
+
+Student::~Student() {
+  cout << "Destructor is called for Student with rollno: " << rollno << endl;
+}
+
+int main() {
+  // Creating a Person object
+  Person p("Mudasir", 17);
+  cout << "Name: " << p.name << endl;
   cout << "Age: " << p.age << endl;
+
+  // Creating a Student object
+  Student s("Ali", 20, 45);
+  cout << "Name: " << s.name << endl;
+  cout << "Age: " << s.age << endl;
+  cout << "Roll No: " << s.rollno << endl;
+
   return 0;
 }
