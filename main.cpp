44d3b7ef1fcb3ca54a33b9a8adc259c7decd2d44
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-29 07:21:20 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,12 @@ using namespace std;
 
 int main() {
     
-    // create a map named student
+   
     map <int, string> student {{1, "Denise"}, {2, "Blake"}, {3, "Courtney"}, {4, "John"}, {5, "Jennifer"}};
     
-    // create map iterator
+
     map <int, string>::iterator itr;
 
-    // display initial map values
     cout << "Initial Values:" << endl;
 
     for(itr = student.begin(); itr != student.end(); ++itr) {
@@ -19,16 +18,16 @@ int main() {
     
     cout << endl;
     
-    // use itr iterator to point to the first map element
+
     itr = student.begin();
 
-    // remove the first element
+
     student.erase(itr);
 
-    // remove the element having key 4
+
     student.erase(4);
 
-    // display final map values
+
     cout << "\nFinal Values:" << endl;
 
     for(itr = student.begin(); itr != student.end(); ++itr) {
