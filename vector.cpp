a023b2a13057afd225967600f681b8199bcc5389
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-30 07:43:16 +0500

    Tree

--- vector.cpp
+++ vector.cpp
@@ -1,37 +1,21 @@
 #include <iostream>
 using namespace std;
 
-// Base class
-class Vehicle {
-public:
-    string brand;
-
-    // Parameterized constructor
-    Vehicle(string vehicleBrand) : brand(vehicleBrand) {
-        cout << "Vehicle constructor called with brand: " << brand << endl;
-    }
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
 };
 
-// Derived class
-class Car : public Vehicle {
-public:
-    string model;
-
-    // Parameterized constructor
-    Car(string carBrand, string carModel) : Vehicle(carBrand), model(carModel) {
-        cout << "Car constructor called with brand: " << carBrand << " and model: " << carModel << endl;
-    }
-
-    // Overloaded constructor
-    Car(string carBrand) : Vehicle(carBrand), model("Unknown") {
-        cout << "Car overloaded constructor called with brand: " << carBrand << endl;
-    }
-};
-
+Node* createNode(int data) {
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->left = newNode->right = nullptr;
+  return newNode;
+}
 int main() {
-    // Creating objects using different constructors
-    Car car1("Toyota", "Corolla");
-    Car car2("Honda");
-
-    return 0;
+    Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+  return 0;
 }
