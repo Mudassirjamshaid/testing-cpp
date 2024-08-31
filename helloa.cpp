8ae2737cf427ff35257179afd6c3fb43db2533bf
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-31 13:58:18 +0500

    updation

--- helloa.cpp
+++ helloa.cpp
@@ -6,7 +6,7 @@ int main() {
     
     map<int, string> student;
   
-    // use [] operator to add elements
+    
     student[1] = "Jacqueline";
     student[2] = "Blake";
 
@@ -14,7 +14,7 @@ int main() {
     student.insert(make_pair(3, "Denise"));
     student.insert(make_pair(4, "Blake"));
 
-    // add elements with duplicate keys
+    
     student[5] = "Timothy";
     student[5] = "Aaron";
 
