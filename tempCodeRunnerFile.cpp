1a999c93eb4750b6707d957d73ffc82180f0117f
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-31 17:33:56 +0500

    updation

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,21 +2,23 @@
 #include <unordered_map>
 using namespace std;
 
-int main(){
-unordered_map<int, string> mystring {
-    {1, "Mudassir"},
-    {2, "Sawera"},
-    {3, "oggy"},
-    {4, "bob"},
-    {5, "Muzammil"},
-    {6, "jack"},
-    {10, "olivia"},
-    {8, "dee dee"},
-    {9, "james"},
-    {7, "josh"},
-};
-    for(auto it = mystring.begin(); it != mystring.end(); it++) {
+int main()
+{
+    unordered_map<int, string> mystring{
+        {1, "Mudassir"},
+        {2, "Sawera"},
+        {3, "oggy"},
+        {4, "bob"},
+        {5, "Muzammil"},
+        {6, "jack"},
+        {10, "olivia"},
+        {8, "dee dee"},
+        {9, "james"},
+        {7, "josh"},
+    };
+    for (auto it = mystring.begin(); it != mystring.end(); it++)
+    {
         cout << it->first << " " << it->second << endl;
     }
-  return 0;
+    return 0;
 }
