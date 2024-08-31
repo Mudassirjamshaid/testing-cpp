6a6f59264de1a16268503e00cc0fd32fa349ed90
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-31 14:17:58 +0500

    testing map

--- helloa.cpp
+++ helloa.cpp
@@ -2,24 +2,22 @@
 #include <map>
 using namespace std;
 
-int main() {
-    
-    map<int, string> student;
-  
-    
-    student[1] = "Jacqueline";
-    student[2] = "Blake";
+int main()
+{
 
-    // use insert() method to add elements
-    student.insert(make_pair(3, "Denise"));
-    student.insert(make_pair(4, "Blake"));
+    map<int, string> student{{1, "oggy"}, {2, "bob"}, {3, "olivia"}, {4, "joey"}};
 
-    
-    student[5] = "Timothy";
-    student[5] = "Aaron";
+    student[1] = "oggy";
+    student[2] = "bob";
+    student[3] = "dee dee";
 
-    for (int i = 1; i <= student.size(); ++i) {
-        cout << "Student[" << i << "]: " << student[i] << endl;
+    student.insert(pair<int, string>(4, "apple"));
+    student.insert(pair<int, string>(5, "orange"));
+    student.insert(pair<int, string>(6, "bnana"));
+
+    for (auto pair : student)
+    {
+        cout << pair.first << " - " << pair.second << endl;
     }
 
     return 0;
