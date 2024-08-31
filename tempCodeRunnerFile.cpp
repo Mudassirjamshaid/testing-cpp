3f1c63b2cbdfd1dd14d763ac6044ee088a9a3c36
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-08-31 17:34:53 +0500

    triangle

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
-#include <unordered_map>
 using namespace std;
 
-int main()
-{
-    unordered_map<int, string> mystring{
-        {1, "Mudassir"},
-        {2, "Sawera"},
-        {3, "oggy"},
-        {4, "bob"},
-        {5, "Muzammil"},
-        {6, "jack"},
-        {10, "olivia"},
-        {8, "dee dee"},
-        {9, "james"},
-        {7, "josh"},
-    };
-    for (auto it = mystring.begin(); it != mystring.end(); it++)
-    {
-        cout << it->first << " " << it->second << endl;
+int main() {
+    int rows;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "* ";
+        }
+        cout << endl;
     }
+
     return 0;
 }
