9907f56c122a16a59d556adf230ab92d88ef4029
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-09-01 09:52:22 +0500

    hello

--- helloa.cpp
+++ helloa.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
-#include <map>
+#include <thread>
+#include <mutex>
+
 using namespace std;
+
+std::mutex mtx;  // Mutex for synchronizing output
+
+void function1()
+{
+    for (int i = 0; i < 200; i++)
+    {
+        std::lock_guard<std::mutex> guard(mtx);  // Lock the mutex
+        cout << "+";
+    }
+}
+
+void function2()
+{
+    for (int i = 0; i < 200; i++)
+    {
+        std::lock_guard<std::mutex> guard(mtx);  // Lock the mutex
+        cout << "-";
+    }
+}
 
 int main()
 {
+    std::thread worker1(function1);
+    std::thread worker2(function2);
+
+    // Join the threads to ensure they complete before exiting
+    worker1.join();
+    worker2.join();
+
+    // Wait for user input before exiting
+    //cin.get();
     return 0;
 }
