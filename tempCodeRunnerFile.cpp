56820240ff3f9002a52e41e3d531300e5ba634e7
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-09-01 08:03:37 +0500

    maping

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,39 +1,19 @@
 #include <iostream>
-#include <thread>
-#include <mutex>
-
+#include <map>
 using namespace std;
-
-std::mutex mtx;  // Mutex for synchronizing output
-
-void function1()
-{
-    for (int i = 0; i < 200; i++)
-    {
-        std::lock_guard<std::mutex> guard(mtx);  // Lock the mutex
-        cout << "*";
-    }
-}
-
-void function2()
-{
-    for (int i = 0; i < 200; i++)
-    {
-        std::lock_guard<std::mutex> guard(mtx);  // Lock the mutex
-        cout << "+";
-    }
-}
 
 int main()
 {
-    std::thread worker1(function1);
-    std::thread worker2(function2);
+    map<int, string> employes {
+        {1, "Mudassir"}, {2, "sawera"},
+        {3, "oggy"}, {4, "Dee Dee"},
+        {5, "Marky"}, {5, "joey"},
+        {6, "jamshaid"}, {9, "Hello"},
+    };
 
-    // Join the threads to ensure they complete before exiting
-    worker1.join();
-    worker2.join();
-
-    // Wait for user input before exiting
+    for(auto pair : employes) {
+        cout << pair.first << " - " << pair.second << endl;
+    }
     cin.get();
     return 0;
 }
