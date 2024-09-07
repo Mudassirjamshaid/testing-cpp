4d7482241e3bf17497e4f2f7af3dbb6c1e8f44c1
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-09-07 19:27:00 +0500

    operator overloding

--- Hello.cpp
+++ Hello.cpp
@@ -13,15 +13,17 @@ struct Hello
     }
 };
 
-void operator<<(ostream& COUT, Hello& yt1) {
+ostream& operator<<(ostream& COUT, Hello& yt1) {
     COUT << "Name: " << yt1.name << endl;
-    COUT << "youTubeChannelCount: " << yt1.youTubeChannelCount << endl;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+    COUT << "youTubeChannelCount: " << yt1.youTubeChannelCount << endl;
+    return COUT;
 };
 
 
 int main()
 {
-    Hello yt1("Mudassir", 2500);
-    cout << yt1;
+    Hello yt1 = Hello("Mudassir", 2500);
+    Hello yt2 = Hello("Muzammil", 80000);
+    cout << yt1 << yt2;
     return 0;
 }
