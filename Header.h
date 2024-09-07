25c93a97a02c43cf9c6ce1be10ebfe6914a5583c
Mudassir jamshaid <mudassirjamshaid9@gmail.com>
2024-09-07 19:07:20 +0500

    updation

--- Header.h
+++ Header.h
@@ -1,29 +1,2 @@
 #pragma once
 #include <iostream>
-
-void calculator();
-void calculator()
-{
-	char op;
-	float num1, num2;
-	cout << "Enter operator (+, -, *, /): ";
-	cin >> op;
-	cout << "Enter two operands: ";
-	cin >> num1 >> num2;
-
-	switch (op)
-	{
-	case '+':
-		cout << num1 + num2 << endl;
-		break;
-	case '-':
-		cout << num1 - num2 << endl;
-		break;
-	case '*':
-		cout << num1 * num2 << endl;
-		break;
-	case '/':
-		if (num2 != 0)
-			cout << num1 / num2 << endl;
-	}
-}
